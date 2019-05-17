6e56482c4e5577f5391bd7a4c34335f7aed4974c
marco <marco@mypc.station>
2019-05-17 21:04:45 +0200

    added back. Euler comparison

--- Examples/Lstability.py
+++ Examples/Lstability.py
@@ -5,18 +5,22 @@ import math as m
 def Fun(t,x):
     return -2000*(x-m.cos(t))
 
-tf=3/2
+tf=1.5
 t0=0
 ts=50
 k=(tf-t0)/ts
-y=np.zeros((ts+1))
+t=np.linspace(t0,tf,ts+1)
+y=np.zeros((ts+1)) #trapz
+yE=np.zeros((ts+1)) #Backward Euler
 y0=0
 y[0]=y0
-t=np.linspace(t0,tf,ts+1)
-
+yE[0]=0
 
 for i in range (0,ts):
-    y[i+1]=(y[i]+1000*k*(y[i]+m.cos(t[i])+m.cos(t[i+1])))/(1-k*1000)
+    y[i+1]=(y[i]+1000*k*(-y[i]+m.cos(t[i])+m.cos(t[i+1])))/(1+k*1000)
+    yE[i+1]=(y[i]+2000*k*m.cos(t[i+1]))/(1+k*2000)
 
-plt.plot(t,y,marker='o')
+plt.plot(t,y,marker='o',label='Trapezi')        
+plt.plot(t,yE,marker='d',label='Backward Euler')
+plt.legend()
 plt.show()
